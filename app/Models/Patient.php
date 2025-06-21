b6887267962b9de8cf33e113d145ee82abc38e60
tajreee <tajrianhaar@gmail.com>
2025-06-21 20:03:51 +0700

    feat: Patient model

---
+++ app/Models/Patient.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Patient extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    protected $table = 'patient';
+
+    protected $fillable = [
+        'user_id',
+        'nik',
+        'birth_date',
+    ];
+
+    protected $casts = [
+        'birth_date' => 'date',
+    ];
+
+    /**
+     * Get the user that owns the Patient
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
