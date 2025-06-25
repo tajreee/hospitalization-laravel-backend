f2a9496b276cc113bb4dc50f647732f568e7f6ad
tajreee <tajrianhaar@gmail.com>
2025-06-25 21:49:13 +0700

    chore: add missing use statements for HasFactory, SoftDeletes, and relationships in Facility, Nurse, and Room models

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Facility extends Model
 {
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
