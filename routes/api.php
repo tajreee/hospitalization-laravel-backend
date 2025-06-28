cd74f63be62afbab2aa4d57e33e53acc3a209a16
tajreee <tajrianhaar@gmail.com>
2025-06-28 23:09:25 +0700

    feat: reorganize API routes for better structure and clarity

--- routes/api.php
+++ routes/api.php
@@ -19,35 +19,34 @@ use App\Http\Controllers\ReservationController; // Pastikan ini ada
 |--------------------------------------------------------------------------
 */
 
-// Patient routes
-Route::prefix('patients')->group(function () {
-    Route::post('/create', [PatientController::class, 'store']);
-    Route::get('/{patient:nik}', [PatientController::class, 'getPatientByNIK']);
+// Public Routes
+Route::post('/auth/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/auth/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+Route::post('/auth/register', function(Request $request) {
+    $role = $request->input('role');
+    
+    if ($role === 'nurse') {
+        return app()->call([NurseController::class, 'create'], $request->all());
+    } else {
+        return app()->call([PatientController::class, 'create'], $request->all());
+    }
 });
 
-// Nurse routes
-Route::prefix('nurses')->group(function () {
-    Route::post('/create', [NurseController::class, 'store']);
+// Patient Routes
+Route::middleware(['auth:api', 'role:patient'])->group(function () {
+    Route::get('/reservations', [ReservationController::class, 'reservations']);
+    Route::get('/rooms/{room:id}', [RoomController::class, 'getRoomById']);
 });
 
-// Room routes
-Route::prefix('rooms')->group(function () {
-    Route::post('/create', [RoomController::class, 'store']);
-    Route::get('/{room:id}', [RoomController::class, 'getRoomById']);
-    Route::delete('/{room:id}/delete', [RoomController::class, 'deleteRoom']);
+// Nurse Routes
+Route::middleware(['auth:api', 'role:nurse'])->group(function () {
+    Route::get('/patients/{patient:nik}', [PatientController::class, 'getPatientByNIK']);
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::post('/facilities/create', [FacilityController::class, 'store']);
+    Route::get('/facilities', [FacilityController::class, 'facilities']);
+    Route::delete('/facilities/{facility:id}/delete', [FacilityController::class, 'deleteFacility']);
+    Route::post('/rooms/create', [RoomController::class, 'store']);
+    Route::get('/rooms/{room:id}', [RoomController::class, 'getRoomById']);
+    Route::delete('/rooms/{room:id}/delete', [RoomController::class, 'deleteRoom']);
 });
-
-// Facility routes
-Route::prefix('facilities')->group(function () {
-    Route::post('/create', [FacilityController::class, 'store']);
-    Route::get('/', [FacilityController::class, 'facilities']);
-    Route::delete('/{facility:id}/delete', [FacilityController::class, 'deleteFacility']);
-});
-
-// Reservation routes
-Route::prefix('reservations')->group(function () {
-    Route::post('/create', [ReservationController::class, 'store']);
-    Route::get('/', [ReservationController::class, 'reservations']);
-});
-
 // ... rute-rute lain jika ada ...
