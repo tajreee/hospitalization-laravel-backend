4cacd7b71786c479a25ee42eb619752a12e29f18
tajreee <tajrianhaar@gmail.com>
2025-07-19 21:54:28 +0700

    fix: correct route handler for getting room reservations to use RoomController

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ Route::middleware(['auth:api', 'role:patient'])->group(function () {
     Route::get('/rooms/{room:id}', [RoomController::class, 'getReservationsBetweenDates']);
     Route::get('/auth/me', [App\Http\Controllers\AuthController::class, 'me']);
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'patientDashboard']);
-    Route::get('/rooms/{room:id}', [ReservationController::class, 'getReservationBetweenDates']);
+    Route::get('/rooms/{room:id}', [RoomController::class, 'getReservationBetweenDates']);
 });
 
 // Nurse Routes
@@ -55,7 +55,7 @@ Route::middleware(['auth:api', 'role:nurse'])->group(function () {
     Route::get('/rooms/available', [RoomController::class, 'getAvailableRooms']);
     Route::post('/rooms/create', [RoomController::class, 'store']);
     // Route::get('/rooms/{room:id}', [RoomController::class, 'getRoomById']);
-    Route::get('/rooms/{room:id}', [ReservationController::class, 'getReservationBetweenDates']);
+    Route::get('/rooms/{room:id}', [RoomController::class, 'getReservationBetweenDates']);
     Route::delete('/rooms/{room:id}/delete', [RoomController::class, 'deleteRoom']);
     Route::get('/auth/me', [App\Http\Controllers\AuthController::class, 'me']);
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'nurseDashboard']);
