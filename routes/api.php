c5b493b4900456c0dd57e01ec58b2b34d82754de
tajreee <tajrianhaar@gmail.com>
2025-07-23 09:01:33 +0700

    fix: add RateLimiter and Limit imports for rate limiting functionality

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
